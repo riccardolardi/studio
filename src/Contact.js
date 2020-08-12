f9820aa068e16f615b5b09827c151348737c54d3
Riccardo Lardi <hello@riccardolardi.com>
2020-08-12 16:54:31 +0200

    upd

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import ReorderIcon from '@material-ui/icons/Reorder';
 import Classnames from 'classnames';
 import './Contact.scss';
 
@@ -23,6 +26,18 @@ function Contact(props) {
           <p>General Guisan-Strasse 78<br/>4054 Basel<br/>Schweiz</p>
           <p><a href="tel:0041793396538">0041 79 339 65 38</a></p>
           <p><a href="mailto:hello@riccardolardi.com">hello@riccardolardi.com</a></p>
+          <a href="https://www.linkedin.com/in/riccardolardi" rel="noopener noreferrer" 
+            target="_blank" className="social-icon" alt="LinkedIn">
+            <LinkedInIcon fontSize={'large'} />
+          </a>
+          <a href="https://github.com/riccardolardi" rel="noopener noreferrer" 
+            target="_blank" className="social-icon" alt="GitHub">
+            <GitHubIcon fontSize={'large'} />
+          </a>
+          <a href="https://stackoverflow.com/users/1515187/riccardolardi" rel="noopener noreferrer" 
+            target="_blank" className="social-icon" alt="StackOverflow">
+            <ReorderIcon fontSize={'large'} />
+          </a>
         </div>
         <div className="image align-left" />
       </article>
