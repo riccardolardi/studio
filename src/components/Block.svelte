3040e8c0b6df74868ac2cdfa21ba2a2dc79e36fe
Riccardo Lardi <riccardo.lardi@gmail.com>
2020-09-27 12:55:23 +0200

    fix instagram bug

--- src/components/Block.svelte
+++ src/components/Block.svelte
@@ -6,12 +6,13 @@
 	export let fill75 = false;
 	export let fill50 = false;
   let blockRef, fillClass, fill75class, fill50class;
+  let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
   $: if (fill) fillClass = $isInsta ? ' block-fill-insta' : ' block-fill';
   $: if (fill75) fillClass = $isInsta ? ' block-fill-75-insta' : ' block-fill-75';
   $: if (fill50) fillClass = $isInsta ? ' block-fill-50-insta' : ' block-fill-50';
-  $: if ($isInsta && fill) blockRef.style.height = `${window.innerHeight}px`;
-  $: if ($isInsta && fill75) blockRef.style.height = `${window.innerHeight * 0.75}px`;
-  $: if ($isInsta && fill50) blockRef.style.height = `${window.innerHeight * 0.5}px`;
+  $: if ($isInsta && fill) blockRef.style.height = `${vh}px`;
+  $: if ($isInsta && fill75) blockRef.style.height = `${vh * 0.75}px`;
+  $: if ($isInsta && fill50) blockRef.style.height = `${vh * 0.5}px`;
 </script>
 
 <style type="text/scss">
