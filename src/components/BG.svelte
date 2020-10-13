0817ad3fd454be546af4185ff2b142d1b215e18c
Riccardo Lardi <riccardo.lardi@gmail.com>
2020-10-13 12:37:41 +0200

    inappbrowser fix

--- src/components/BG.svelte
+++ src/components/BG.svelte
@@ -30,7 +30,6 @@
 		pixelRatio = window.devicePixelRatio;
 		winW = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 		winH = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-		if ($isInAppBrowser) winH /= pixelRatio;
 		aspect = winW / winH;
 		frustumSize = 5;
 		bgEl = document.querySelector('#BG');
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -47,13 +47,6 @@
 			}
 	  </style>
 	{/if}
-	{#if $isInAppBrowser}
-		<style>
-			.block-fill {
-				min-height: 624px !important;
-			}
-		</style>
-	{/if}
 </svelte:head>	
 
 <style type="text/scss" global>
