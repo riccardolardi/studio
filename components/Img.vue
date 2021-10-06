3a70358430a36096906e54ec784ef3034d245c3c
Riccardo Lardi <hello@riccardolardi.com>
2021-10-06 13:04:23 +0200

    rename image component

---
+++ components/Img.vue
@@ -0,0 +1,52 @@
+<template>
+  <nuxt-picture
+    ref="image"
+    format="webp"
+    quality="80"
+    sizes="sm:320px md:480px lg:640px"
+    :src="src"
+    :width="width"
+    :height="height"
+    :alt="alt"
+    loading="lazy"
+    class="Img"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Img',
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: null,
+      default: 'auto',
+    },
+    height: {
+      type: null,
+      default: 'auto',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.Img {
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
